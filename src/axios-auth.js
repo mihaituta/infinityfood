186a095c8e30f5fc7da5ecb5462d2864e5cb01fc
Mihai Tuță <tutamihai@gmail.com>
2021-09-13 17:43:35 +0200

    Changed images link to heroku api url

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     baseURL: 'https://laravel-api-food.herokuapp.com'
+    // baseURL: 'http://api-food.com'
 });
 
 const token = localStorage.getItem('token');
